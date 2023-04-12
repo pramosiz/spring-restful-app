3129c968eebd867726e2e836d3610e4a908bd1aa
Pablo <ramosizquierdop@gmail.com>
2023-04-12 20:53:48 +0200

    car-service microservice

---
+++ car-service/src/main/java/com/tutorial/carservice/controller/CarController.java
@@ -0,0 +1,57 @@
+package com.tutorial.carservice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tutorial.carservice.entity.Car;
+import com.tutorial.carservice.service.CarService;
+
+@RestController
+@RequestMapping("/car")
+public class CarController {
+
+	@Autowired
+	CarService carService;
+	
+	@GetMapping
+	public ResponseEntity<List<Car>> getAll() {
+		List<Car> cars = carService.getAll();
+		if(cars.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(cars);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Car> getById(@PathVariable("id") int id) {
+		Car car = carService.getUserById(id);
+		if(car == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(car);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Car> save(@RequestBody Car car) {
+		Car carNew = carService.save(car);
+		return ResponseEntity.ok(carNew);
+	}
+	
+	@GetMapping("/byUser/{userId}")
+	public ResponseEntity<List<Car>> getByUserId(@PathVariable("userId") int userId) {
+		List<Car> cars = carService.byUserId(userId);
+		if(cars.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.ok(cars);
+	}
+	
+}
